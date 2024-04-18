c59a66a8723977c7057143aba23708acc108f643
Jachymeqq <96845626+Jachymeqq@users.noreply.github.com>
2024-04-18 13:35:51 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,24 +1,22 @@
 fx_version 'adamant'
 games { 'gta5' }
 
-ui_page 'html/index.html'
-
 author 'Jachymeqq'
-description 'Insert Script' -- https://danny255-scripts.tebex.io/package/4383179
+description 'Insert Script'
 version '1.0.0'
 
 
 client_scripts {
    '@es_extended/locale.lua',
-	'locales/*.lua',
+   'locales/*.lua',
    'config.lua',
-	'client.lua',
+   'client.lua',
 }
 
 
 server_scripts {
    '@es_extended/locale.lua',
-	'locales/*.lua',
+   'locales/*.lua',
    'config.lua',
    'server.lua',
 }
