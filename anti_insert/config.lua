7c8dca09978ed7d2cb372358800b874b80b52f03
Jachymeqq <96845626+Jachymeqq@users.noreply.github.com>
2024-04-18 13:30:11 +0200

    Add files via upload

---
+++ anti_insert/config.lua
@@ -0,0 +1,9 @@
+Config = {}
+
+Config.webhook = "YOUR_WEBHOOK" 
+
+Config.dropplayer = false   -- do you want to kick player when they press insert?
+
+Config.BotName = "YOUR_DISCORD_BOT_NAME" 
+
+Config.Locale = 'en'   -- en/cs
---
+++ anti_insert/locales/cs.lua
@@ -0,0 +1,7 @@
+Locales['cs'] = {
+  ['pressed_insert_kick'] = 'Stisknul jsi insert!',
+  ['pressed_insert_message'] = 'Tento hráč stisknul klávesu Insert!',
+  ['player_id'] = 'ID Hráče',
+  ['player_name'] = 'Jméno Hráče',
+}
+   
